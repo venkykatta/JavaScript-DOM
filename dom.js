23e5d528b94f83912430c737a695b390f3b81e36
venkykatta <venkateshkatta9705@gmail.om>
2021-11-05 23:54:11 +0530

    Document Object Model javascript file

---
+++ dom.js
@@ -0,0 +1,34 @@
+// console.log(document.designMode)
+// console.log(document.URL)
+// console.log(document.body)
+// console.log(document.head)
+// console.log(document.all[8])
+
+// console.log(document.images)
+
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle)
+// headerTitle.textContent = 'Venky';
+// headerTitle.innerText = 'good';
+// headerTitle.innerHTML = '<h3>Hello</h3>'
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'venky';
+// items[1].style.fontWeight = 'bold';
+// items[3].style.backgroundColor = 'yellow';
+
+// for (var i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = 'orange';
+// }
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 10px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hi Venky!!';
+
+
+var titles = document.querySelectorAll('.title');
+console.log(titles)
