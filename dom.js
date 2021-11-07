78eeaec02b3d4f4c0829f8bb353bd7e352a07d22
venkykatta <venkateshkatta9705@gmail.om>
2021-11-07 21:47:50 +0530

    Adding a few more EventListeners to the file

--- dom.js
+++ dom.js
@@ -33,7 +33,7 @@
 // var titles = document.querySelectorAll('.title');
 // console.log(titles)
 
-var itemList = document.querySelector('#items');
+// var itemList = document.querySelector('#items');
 
 // console.log(itemList.parentNode.parentNode.parentNode);
 // console.log(itemList.parentNode);
@@ -56,16 +56,62 @@ var itemList = document.querySelector('#items');
 // console.log(itemList.previousSibling);
 
 // create div
-var newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'helloo';
-newDiv.setAttribute('title', 'Hello Div');
+// var newDiv = document.createElement('div');
+// newDiv.className = 'hello';
+// newDiv.id = 'helloo';
+// newDiv.setAttribute('title', 'Hello Div');
 
-var newDivText = document.createTextNode("Hello venky!");
-newDiv.appendChild(newDivText);
+// var newDivText = document.createTextNode("Hello venky!");
+// newDiv.appendChild(newDivText);
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-console.log(newDiv);
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+// console.log(newDiv);
 
-container.insertBefore(newDiv, h1);
+// container.insertBefore(newDiv, h1);
+
+// var button = document.getElementById('button').addEventListener('click', buttonClick)
+
+// function buttonClick(e) {
+// console.log("Button Clicked!")
+// document.getElementById('header-title').textContent = 'venky';
+// document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+// console.log(e);
+// console.log(e.target);
+// console.log(e.target.className);
+// console.log(e.target.id);
+// console.log(e.target.classList);
+//     var output = document.getElementById('output');
+//     output.innerHTML = '<h3>' + e.target.id + '</h3>';
+
+//     console.log(e.type);
+// }
+
+// var button = document.getElementById('button').addEventListener('click', runEvent);
+
+// function runEvent(e) {
+//     console.log('EVENT TYPE: ' + e.type);
+// }
+
+var button = document.getElementById("button");
+var box = document.getElementById("box");
+
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);\
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+box.addEventListener('mousemove', runEvent);
+
+function runEvent(e) {
+    console.log('EVENT TYPE:' + e.type);
+
+    // output.innerHTML = '<h3>Mouse x: ' + e.offsetX + '</h3><h3>Mouse Y: ' + e.offsetY + '</h3>';
+    box.style.backgroundColor = "rgb(" + e.offsetX + "," + e.offsetY + ",40)";
+}
